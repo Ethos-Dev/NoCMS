fbe0b2edfc9246735519462ede15e8218f1b2797
Ethos <tarek.chayya@ethoskms.com>
2017-10-16 18:06:47 +0300

    Signed-off-by: Ethos <tarek.chayya@ethoskms.com>

--- The Site/assets/js/main.js
+++ The Site/assets/js/main.js
@@ -3,6 +3,6 @@
 $(function() {
 	var thebody=$("#automated");
 	for(var i=1; i<6; i++){
-		thebody.append("<h"+i+">Welcome again ("+i+")</h"+i+">");
+		thebody.append("<h"+i+">Welcome again (-"+i+"-)</h"+i+">");
 	}
 });
